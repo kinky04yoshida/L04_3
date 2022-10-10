4028b3b3a0cf40ab395c77c4434348822d237a54
Ubuntu <ubuntu@ip-172-31-74-155.ec2.internal>
2022-10-10 11:12:52 +0000

    upload

---
+++ a.rb
@@ -0,0 +1,17 @@
+require 'bcrypt'
+require 'dbi'
+#signup_password = BCrypt::Password.create("my password")
+
+#user = User.find(uid: params[:uid]) uid=nilじゃないならpassチェック
+#login_password = BCrypt::Password.new(user.pass)
+
+#login_password = BCrypt::Password.new(signup_password)
+#if login_password == params[:pass]
+#    if login_password == "my password" #←login_password.==(“my password”)
+#        p "ログイン成功"
+#    end
+
+
+
+
+
